cbe9c0d49b61ae5ac67ed9b353fa1643b7269497
Tobias Schürg <tobiasschuerg@gmail.com>
2016-08-14 08:03:44 +0200

    FloatMath fix

--- src/com/github/andlyticsproject/cache/LRUBitmapCache.java
+++ src/com/github/andlyticsproject/cache/LRUBitmapCache.java
@@ -1,14 +1,13 @@
 
 package com.github.andlyticsproject.cache;
 
+import android.graphics.Bitmap;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import android.graphics.Bitmap;
-import android.util.FloatMath;
-import android.util.Log;
 
 public class LRUBitmapCache {
 
@@ -23,12 +22,11 @@ public class LRUBitmapCache {
 	/**
 	 * Creates a new LRU cache.
 	 *
-	 * @param cacheSize
-	 *            the maximum number of entries that will be kept in this cache.
+	 * @param cacheSize the maximum number of entries that will be kept in this cache.
 	 */
 	public LRUBitmapCache(int cacheSize) {
 		this.cacheSize = cacheSize;
-		int hashTableCapacity = (int) FloatMath.ceil(cacheSize / hashTableLoadFactor) + 1;
+		int hashTableCapacity = (int) Math.ceil(cacheSize / hashTableLoadFactor) + 1;
 		map = new LinkedHashMap<String, Bitmap>(hashTableCapacity, hashTableLoadFactor, true) {
 			// (an anonymous inner class)
 			private static final long serialVersionUID = 1;
@@ -38,8 +36,7 @@ public class LRUBitmapCache {
 				boolean result = size() > LRUBitmapCache.this.cacheSize;
 				if (result) {
 					if (eldest.getValue() != null) {
-						Log.d(TAG, "Recycling bitmap: " + eldest.getKey() + " current cache size: "
-								+ size());
+						Log.d(TAG, "Recycling bitmap: " + eldest.getKey() + " current cache size: " + size());
 					}
 				}
 				return result;
@@ -51,10 +48,9 @@ public class LRUBitmapCache {
 	 * Retrieves an entry from the cache.<br>
 	 * The retrieved entry becomes the MRU (most recently used) entry.
 	 *
-	 * @param key
-	 *            the key whose associated value is to be returned.
+	 * @param key the key whose associated value is to be returned.
 	 * @return the value associated to this key, or null if no value with this
-	 *         key exists in the cache.
+	 * key exists in the cache.
 	 */
 	public synchronized Bitmap get(String key) {
 		return map.get(key);
@@ -66,10 +62,8 @@ public class LRUBitmapCache {
 	 * cache, it is replaced by the new entry. If the cache is full, the LRU
 	 * (least recently used) entry is removed from the cache.
 	 *
-	 * @param key
-	 *            the key with which the specified value is to be associated.
-	 * @param value
-	 *            a value to be associated with the specified key.
+	 * @param key   the key with which the specified value is to be associated.
+	 * @param value a value to be associated with the specified key.
 	 */
 	public synchronized void put(String key, Bitmap value) {
 		map.put(key, value);
@@ -102,7 +96,7 @@ public class LRUBitmapCache {
 	 * @return a <code>Collection</code> with a copy of the cache content.
 	 */
 	public synchronized Collection<Map.Entry<String, Bitmap>> getAll() {
-		return new ArrayList<Map.Entry<String, Bitmap>>(map.entrySet());
+		return new ArrayList<>(map.entrySet());
 	}
 
 }
